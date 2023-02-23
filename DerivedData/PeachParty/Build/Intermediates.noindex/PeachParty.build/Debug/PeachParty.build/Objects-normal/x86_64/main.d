835e6e5b8c343646190fa481ab1b922959aae5b1
RyanMYang <ryan.m.yang@gmail.com>
2023-02-23 12:22:22 -0800

    removed DerivedData

--- DerivedData/PeachParty/Build/Intermediates.noindex/PeachParty.build/Debug/PeachParty.build/Objects-normal/x86_64/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/ryanmacpro/Documents/School/CS32/PeachParty/PeachParty/main.cpp \
-  /Users/ryanmacpro/Documents/School/CS32/PeachParty/PeachParty/GameController.h \
-  /Users/ryanmacpro/Documents/School/CS32/PeachParty/PeachParty/SpriteManager.h \
-  /usr/X11/include/GL/freeglut.h /usr/X11/include/GL/freeglut_std.h \
-  /usr/X11/include/GL/freeglut_ext.h \
-  /usr/X11/include/GL/freeglut_ucall.h \
-  /Users/ryanmacpro/Documents/School/CS32/PeachParty/PeachParty/GameConstants.h
